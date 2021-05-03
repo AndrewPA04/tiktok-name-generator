44b8d1ce55b75d2af8dd973aaa7ab48a9433079e
qeiray <62768543+AndrewPA04@users.noreply.github.com>
2021-05-03 06:39:03 +0300

    Update tiktokNameGenerator.py

--- tiktokNameGenerator.py
+++ tiktokNameGenerator.py
@@ -1,20 +1,17 @@
-import requests
-import asyncio
-import random
-import string
+from requests import get
+from random import choice
+from string import ascii_letters
 
-async def generator():
-        counter = 0
-        while(counter<500):
-            generatedName = ""
-            counter += 1
-            for index in range(4):
-                randomChar = random.choice(string.ascii_letters)
-                generatedName += randomChar
-            response = requests.get(f"https://www.tiktok.com/@{generatedName}")
-            if response.status_code == 200:
-                print(f"[taken] {generatedName}")
-            else:
-                print(f"[Not taken] {generatedName}")
-
-asyncio.run(generator())
+def generator():
+    counter = 0
+    while(counter<500):
+        generatedName = ""
+        counter += 1
+        for index in range(4):
+            randomChar = choice(ascii_letters)
+            generatedName += randomChar
+        response = get(f"https://www.tiktok.com/@{generatedName}")
+        if response.status_code == 200:
+            print(f"[taken] {generatedName}")
+        else:
+            print(f"[Not taken] {generatedName}")
